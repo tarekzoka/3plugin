59ac7c95ab968c7a95771a3f9f31205ca856eef4
tarekhanfy <65197356+tarekzoka@users.noreply.github.com>
2021-09-25 05:50:01 +0200

    Add files via upload

---
+++ installer.sh
@@ -0,0 +1,27 @@
+#!/bin/sh
+# ###########################################
+# SCRIPT : DOWNLOAD AND INSTALL 3plugin
+
+# ###########################################
+#
+# Command: wget https://raw.githubusercontent.com/tarekzoka/3plugin/main/installer.sh -O - | /bin/sh #
+#
+# ###########################################**
+
+###########################################
+# Configure where we can find things here #
+
+MY_URL='https://raw.githubusercontent.com/tarekzoka/main/3plugin'
+
+####################
+#  DOWNLOAD AND INSTALL  #
+
+opkg install --force-overwrite  https://github.com/tarekzoka/3plugin/raw/main/enigma2-plugin-extensions-jedimakerxtream_6.18_all.ipk
+wait
+opkg install --force-overwrite  https://github.com/tarekzoka/3plugin/raw/main/enigma2-plugin-extensions-xcplugin-forever_1.7_all.ipk
+wait
+https://github.com/tarekzoka/3plugin/raw/main/enigma2-plugin-extensions-xstreamity_3.18.20210917_all.ipk
+wait
+sleep 2;
+exit 0
+
